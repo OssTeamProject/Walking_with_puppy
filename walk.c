fbd4d9673b62b446632012220bc605db113895e0
s22000593 <103638613+22000593lee@users.noreply.github.com>
2022-05-07 22:20:40 +0900

    update 기능 수정

--- walk.c
+++ walk.c
@@ -27,15 +27,15 @@ void readWalk(Walk w) {
 }
 
 int updateWalk(Walk *w) {
-    printf("산책한 날짜는? (월/일)\n");
+    printf("새 산책한 날짜는? (월/일)\n");
     getchar();
     scanf("%[^\n]s", w->day);
-    printf("산책한 장소는?\n");
+    printf("새 산책한 장소는?\n");
     getchar();
     scanf("%[^\n]s", w->place);
-    printf("산책한 시간은? (분)\n");
+    printf("새 산책한 시간은? (분)\n");
     scanf("%d", &w->time);
-    printf("날씨는? (1:맑음 2:흐림 3:비 4:눈)\n");
+    printf("새 날씨는? (1:맑음 2:흐림 3:비 4:눈)\n");
     scanf("%d", &w->weather);
     
     return 1;
